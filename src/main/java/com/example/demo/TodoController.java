360272ddec75aaf6709516757651e25982db75b1
LAPTOP-OELQH1V6\academia <202511_aw_sato@x-ide.sakura.ne.jp>
2026-02-27 10:48:52 +0900

    7-3

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -7,9 +7,11 @@ import com.example.demo.service.TodoService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/todo")
@@ -48,6 +50,21 @@ public class TodoController {
         todo.setTitle(title);
         todo.setCompleted(Boolean.FALSE);
         todoService.insert(todo);
+        return "redirect:/todo";
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+        try {
+            boolean deleted = todoService.deleteById(id);
+            if (deleted) {
+                redirectAttributes.addFlashAttribute("successMessage", "\u0054\u006f\u0044\u006f\u3092\u524a\u9664\u3057\u307e\u3057\u305f");
+            } else {
+                redirectAttributes.addFlashAttribute("errorMessage", "\u524a\u9664\u306b\u5931\u6557\u3057\u307e\u3057\u305f");
+            }
+        } catch (RuntimeException ex) {
+            redirectAttributes.addFlashAttribute("errorMessage", "\u524a\u9664\u306b\u5931\u6557\u3057\u307e\u3057\u305f");
+        }
         return "redirect:/todo";
     }
 }
